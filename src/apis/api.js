2d31387e480b82313469b242cce7baf51f1f59b7
이민희 <mhl98@dgu.ac.kr>
2023-06-23 23:09:16 +0900

    feat: api 추가

---
+++ src/apis/api.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+const baseURL =
+  "http://ec2-54-180-201-100.ap-northeast-2.compute.amazonaws.com:8080";
+
+const request = async ({ url, method, body, params, token }) => {
+  try {
+    const config = {
+      baseURL,
+      params,
+      headers: {},
+    };
+    if (token && config.headers) {
+      config.headers["Authorization"] = token;
+    }
+    const data =
+      (method === "get" && (await axios.get(url, config))) ||
+      (method === "post" && (await axios.post(url, body, config))) ||
+      (method === "patch" && (await axios.patch(url, body, config))) ||
+      (method === "put" && (await axios.put(url, body, config))) ||
+      (method === "delete" &&
+        (await axios.delete(url, {
+          baseURL,
+          params,
+          headers: { Authorization: token },
+          data: body,
+        }))) ||
+      {};
+    return data;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+export const GET = (url, token) => request({ url, method: "get", token });
+export const POST = (url, body, token) =>
+  request({ url, method: "post", body, token });
+export const PATCH = (url, body, token) =>
+  request({ url, method: "patch", body, token });
+export const PUT = (url, body, token) =>
+  request({ url, method: "put", body, token });
+export const DELETE = (url, body, token) =>
+  request({ url, method: "delete", body, token });
---
+++ src/apis/user.js
@@ -0,0 +1,61 @@
+import { POST } from "./api";
+import { setToken } from "@/modules/setToken";
+import { getCookie } from "@/modules/cookie";
+
+/* 소셜 로그인 */
+export const getKakaoToken = async (code) => {
+  const { data } = await POST(`/oauth/login/kakao?code=${code}`);
+  setToken({ accessToken: data.accessToken, grantType: data.grantType });
+};
+
+export const postKakaoLogout = async () => {
+  console.log(getCookie("accessToken"));
+  const { data } = await POST(
+    `/oauth/logout/kakao`,
+    {},
+    getCookie("accessToken")
+  );
+  return data;
+};
+
+export const postNaverLogin = async (code) => {
+  const { data } = await POST(`/oauth/login/naver?code=${code}`);
+  console.log(data);
+};
+
+export const postNaverLogout = async () => {
+  console.log(getCookie("accessToken"));
+  const { data } = await POST(
+    `/oauth/logout/naver`,
+    {},
+    getCookie("accessToken")
+  );
+  return data;
+};
+
+export const postGoogleLogin = async (code) => {
+  const { data } = await POST(`/oauth/login/google?code=${code}`);
+  setToken({ accessToken: data.accessToken, grantType: data.grantType });
+};
+
+export const postGoogleLogout = async () => {
+  console.log(getCookie("accessToken"));
+  const { data } = await POST(
+    `/oauth/logout/google`,
+    {},
+    getCookie("accessToken")
+  );
+  return data;
+};
+
+/* 일반 로그인 */
+export const postLogin = async (body) => {
+  const { data } = await POST("/login", body);
+  return data;
+};
+
+/* 회원가입 */
+export const postSignUp = async (body) => {
+  const { data } = await POST("/accounts/new", body);
+  return data;
+};
