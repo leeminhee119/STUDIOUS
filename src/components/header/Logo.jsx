83b9ed3e633f1b3c3d925d327e9b7ff9ca6927a7
이민희 <mhl98@dgu.ac.kr>
2023-07-04 00:14:15 +0900

    refactor: 로고 컴포넌트 생성, 클릭 시 이동

---
+++ src/components/header/Logo.jsx
@@ -0,0 +1,13 @@
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+export default function Logo() {
+  const navigate = useNavigate();
+  return <LogoBox onClick={() => navigate("/")}>STUDIOUS</LogoBox>;
+}
+
+const LogoBox = styled.div`
+  cursor: pointer;
+  ${({ theme }) => theme.fonts.logo};
+  color: ${({ theme }) => theme.colors.mainDark};
+`;
