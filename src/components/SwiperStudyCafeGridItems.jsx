7c1ca7cd780052fe294947dc5ef1ff70766a6a72
이민희 <mhl98@dgu.ac.kr>
2023-07-25 23:35:18 +0900

    feat: 메인페이지 스와이퍼 추가

---
+++ src/components/SwiperStudyCafeGridItems.jsx
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+import StudyCafeGridItem from "./StudyCafeGridItem";
+
+const SwiperStudyCafeGridItems = ({ items, title }) => {
+  return (
+    <SwiperLayout>
+      <SwiperTitle>{title}</SwiperTitle>
+      <Swiper
+        slidesPerView={3.5}
+        spaceBetween={10}
+        slidesPerColumnFill="row"
+        grabCursor={true}
+        autoplay={{ delay: 3000 }}
+        navigation={true}
+        modules={[Navigation]}
+      >
+        {items.map((item, itemIndex) => (
+          <SwiperSlide key={itemIndex}>
+            <StudyCafeGridItem item={item} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </SwiperLayout>
+  );
+};
+
+export default SwiperStudyCafeGridItems;
+
+const SwiperLayout = styled.section`
+  margin-bottom: 5rem;
+`;
+
+const SwiperTitle = styled.div`
+  ${({ theme }) => theme.fonts.heading1Bold}
+  margin-bottom: 1.2rem;
+`;
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,5 +1,95 @@
+import SwiperStudyCafeGridItems from "components/SwiperStudyCafeGridItems";
+
+const DUMMY_DATA = [
+  {
+    cafeName: "스캇",
+    photo: null,
+    distance: "혜화역 도보 5분",
+    grade: 4.7,
+    hashtags: ["청결한", "친절한"],
+    accumRevCnt: 2322,
+  },
+  {
+    cafeName: "스캇",
+    photo: null,
+    distance: "혜화역 도보 5분",
+    grade: 4.7,
+    hashtags: ["청결한", "친절한"],
+    accumRevCnt: 2322,
+  },
+  {
+    cafeName: "스캇",
+    photo: null,
+    distance: "혜화역 도보 5분",
+    grade: 4.7,
+    hashtags: ["청결한", "친절한"],
+    accumRevCnt: 2322,
+  },
+  {
+    cafeName: "스캇",
+    photo: null,
+    distance: "혜화역 도보 5분",
+    grade: 4.7,
+    hashtags: ["청결한", "친절한"],
+    accumRevCnt: 2322,
+  },
+  {
+    cafeName: "스캇",
+    photo: null,
+    distance: "혜화역 도보 5분",
+    grade: 4.7,
+    hashtags: ["청결한", "친절한"],
+    accumRevCnt: 2322,
+  },
+  {
+    cafeName: "스캇",
+    photo: null,
+    distance: "혜화역 도보 5분",
+    grade: 4.7,
+    hashtags: ["청결한", "친절한"],
+    accumRevCnt: 2322,
+  },
+  {
+    cafeName: "스캇",
+    photo: null,
+    distance: "혜화역 도보 5분",
+    grade: 4.7,
+    hashtags: ["청결한", "친절한"],
+    accumRevCnt: 2322,
+  },
+  {
+    cafeName: "스캇",
+    photo: null,
+    distance: "혜화역 도보 5분",
+    grade: 4.7,
+    hashtags: ["청결한", "친절한"],
+    accumRevCnt: 2322,
+  },
+  {
+    cafeName: "스캇",
+    photo: null,
+    distance: "혜화역 도보 5분",
+    grade: 4.7,
+    hashtags: ["청결한", "친절한"],
+    accumRevCnt: 2322,
+  },
+];
+
 const Main = () => {
-  return <></>;
+  return (
+    <>
+      <SwiperStudyCafeGridItems
+        items={DUMMY_DATA}
+        title={"오늘의 추천 스터디카페"}
+      />
+      <SwiperStudyCafeGridItems
+        items={DUMMY_DATA}
+        title={"이벤트 중인 스터디카페"}
+      />
+    </>
+  );
 };
 
 export default Main;
+
+// export
