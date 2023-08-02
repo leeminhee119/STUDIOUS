232709e498291f7dbb9b31c2438f5357656dac39
이민희 <mhl98@dgu.ac.kr>
2023-08-02 23:29:45 +0900

    refactor: nav-bar 제어권 상위로 이동

--- src/pages/StudyCafeDetails.jsx
+++ src/pages/StudyCafeDetails.jsx
@@ -1,12 +1,41 @@
-import styled from "styled-components";
+import { useState } from "react";
 import CommonInformation from "components/studyCafeDetails/commonInfomation/CommonInformation";
 import NavBar from "components/studyCafeDetails/NavBar";
+import StudyRoomReservation from "components/studyCafeDetails/studyRoomReservation/StudyRoomReservation";
 
 const StudyCafeDetails = () => {
+  const NAVBAR_CONTENTS = [
+    {
+      name: "스터디룸 예약",
+      component: <StudyRoomReservation />,
+    },
+    {
+      name: "리뷰",
+      component: <StudyRoomReservation />,
+    },
+    {
+      name: "진행 중인 이벤트",
+      component: <StudyRoomReservation />,
+    },
+    {
+      name: "환불 정책",
+      component: <StudyRoomReservation />,
+    },
+    {
+      name: "유의사항",
+      component: <StudyRoomReservation />,
+    },
+  ];
+  const [activeIndex, setActiveIndex] = useState(0);
   return (
     <>
       <CommonInformation />
-      <NavBar />
+      <NavBar
+        navBarItems={NAVBAR_CONTENTS.map((content) => content.name)}
+        onClickMenu={(idx) => setActiveIndex(idx)}
+        activeIndex={activeIndex}
+      />
+      {NAVBAR_CONTENTS[activeIndex].component}
     </>
   );
 };
