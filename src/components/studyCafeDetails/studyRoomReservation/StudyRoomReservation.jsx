6dbaeb2a406b17ffd36a9fa353b6f0118548fecf
이민희 <mhl98@dgu.ac.kr>
2023-08-03 00:51:33 +0900

    feat: 스터디룸탭 예약일자 선택

--- src/components/studyCafeDetails/studyRoomReservation/StudyRoomReservation.jsx
+++ src/components/studyCafeDetails/studyRoomReservation/StudyRoomReservation.jsx
@@ -1,5 +1,41 @@
+import styled from "styled-components";
+import { useState } from "react";
+import TabContainer from "../TabContainer";
+import Calendar from "components/Search/Calendar";
+import { ReactComponent as CalendarIcon } from "assets/icons/calendar.svg";
+
 const StudyRoomReservation = () => {
-  return <></>;
+  const [isShowCalendar, setIsShowCalendar] = useState(false);
+  const [selectedDate, setSelectedDate] = useState(
+    new Date().toLocaleDateString()
+  );
+  return (
+    <>
+      <TabContainer title={"스터디룸 예약"}>
+        <SelectDateBox>
+          <span>예약일자</span>
+          <div onClick={() => setIsShowCalendar(() => !isShowCalendar)}>
+            <span>{selectedDate}</span>
+            <CalendarIcon />
+          </div>
+        </SelectDateBox>
+        {isShowCalendar && <Calendar onSelectDate={() => {}} />}
+      </TabContainer>
+    </>
+  );
 };
 
 export default StudyRoomReservation;
+
+const SelectDateBox = styled.div`
+  ${({ theme }) => theme.fonts.heading2}
+  display: flex;
+  gap: 3rem;
+  align-items: center;
+
+  > div {
+    display: flex;
+    gap: 2rem;
+    align-items: center;
+  }
+`;
