523eb08e0e541ba1740057fb78add223b94b237f
이민희 <mhl98@dgu.ac.kr>
2023-08-23 00:48:37 +0900

    fix: api 경로 수정

--- src/apis/user.js
+++ src/apis/user.js
@@ -3,24 +3,35 @@ import { getCookie } from "utils/cookie";
 
 /* 소셜 로그인 */
 export const postOAuthLogin = async (code, platform) => {
-  const { data } = await POST(`/oauth/authenticate/${platform}?code=${code}`);
+  const { data } = await POST(
+    `/studious/oauth/authenticate/${platform}?code=${code}`
+  );
   return data;
 };
 
 /* 일반 로그인 */
 export const postLogin = async (body) => {
-  const { data } = await POST("/members/login", body);
+  const { data } = await POST("/studious/members/login", body);
   return data;
 };
 
 /* 회원가입 */
 export const postSignUp = async (body, failCallback) => {
-  const { data } = await POST("/members/signup", body, "", failCallback);
+  const { data } = await POST(
+    "/studious/members/signup",
+    body,
+    "",
+    failCallback
+  );
   return data;
 };
 
 /* 로그아웃 */
 export const postLogout = async () => {
-  const { data } = await POST("/members/logout", {}, getCookie("accessToken"));
+  const { data } = await POST(
+    "/studious/members/logout",
+    {},
+    getCookie("accessToken")
+  );
   return data;
 };
